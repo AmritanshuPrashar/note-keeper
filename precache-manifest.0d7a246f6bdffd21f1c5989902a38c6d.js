1982d71bd80f51cfe3ead4c8813f19298af13f15
Amritanshu <amritanshu1002@gmail.com>
2022-06-21 20:53:29 +0530

    Updates

---
+++ precache-manifest.0d7a246f6bdffd21f1c5989902a38c6d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d337a9c14637da2ac4e17cdbb2c16608",
+    "url": "/note-keeper/index.html"
+  },
+  {
+    "revision": "f2e32efa04ae25099e77",
+    "url": "/note-keeper/static/js/2.c4ea23dd.chunk.js"
+  },
+  {
+    "revision": "a78bd8608e69e801ed20",
+    "url": "/note-keeper/static/js/main.ea3eb96a.chunk.js"
+  },
+  {
+    "revision": "5c09552dff8737d2174e",
+    "url": "/note-keeper/static/js/runtime-main.4bdd423f.js"
+  }
+]);
